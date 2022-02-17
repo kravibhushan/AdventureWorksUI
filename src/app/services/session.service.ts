5752f9b4ececac597b700acc6c2f6a864d8e627f
Ravi Bhushan Kumar <ravibhushan.kumar@outlook.com>
2022-02-17 11:42:17 +0100

    Menu css fix for the toggle

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class SessionService {
 
   constructor() { }
+  
   getUserToken(): string | undefined {
     let userToken: string | undefined = sessionStorage.getItem('userToken')?.toString();
     return userToken;
